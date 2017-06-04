dc37910dd1268d5aa66c88e30ffff44c60232b4c
Victor Nunes <victor.marcelino.nunes@usp.br>
2017-06-04 17:50:43 -0300

    Create a icon

--- app/src/main/java/com/example/victor/fastru/LoginActivity.java
+++ app/src/main/java/com/example/victor/fastru/LoginActivity.java
@@ -2,12 +2,23 @@ package com.example.victor.fastru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // @TODO: Bloquear tela no modo retrato
+    // @TODO: Tela "Opção de compra"
+    // @TODO: Tela "Pagamento"
+    // @TODO: Tela "Voucher"
+    // @TODO: Fazer um ícone mais bonito
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onIniciarClick(View view) {
+
     }
 }
