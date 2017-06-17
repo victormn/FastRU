244c012584c954db1b4d8c0bf97e5a20bd3282d7
Victor Nunes <victor.marcelino.nunes@usp.br>
2017-06-17 17:43:27 -0300

    Create logic to change activities and little changes
    * Now is not allowed to change the orientation
    * Change icon

--- app/src/main/java/com/example/victor/fastru/LoginActivity.java
+++ app/src/main/java/com/example/victor/fastru/LoginActivity.java
@@ -1,24 +1,25 @@
 package com.example.victor.fastru;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
-    // @TODO: Bloquear tela no modo retrato
     // @TODO: Tela "Pagamento"
-    // @TODO: Fazer um ícone mais bonito
-    // @TODO: Fazer lógicas entre as telas
     // @TODO: Ver como será a transição da última tela para a principal
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
     }
 
     public void onIniciarClick(View view) {
-
+        Intent intent = new Intent(this, ShopActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/victor/fastru/ShopActivity.java
+++ app/src/main/java/com/example/victor/fastru/ShopActivity.java
@@ -1,7 +1,10 @@
 package com.example.victor.fastru;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ShopActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class ShopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+    }
+
+    public void onIniciarClick(View view) {
+        Intent intent = new Intent(this, VoucherActivity.class);
+        startActivity(intent);
     }
 }
